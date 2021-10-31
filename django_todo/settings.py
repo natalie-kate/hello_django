8ac5dcd17836e6c40d61136e77d53bf4f29e47f9
Natalie Kate Alexander <nataliekatealexander@gmail.com>
2021-10-31 15:46:33 +0000

    Add if/else to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tnrvh-x3+*#!w_29pk!g3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
